6507f78db378ef514abf1e5ca643f5446d52a706
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-06-06 00:03:59 +0200

    added Swagger docs for the API

--- cmd/ibanator/main.go
+++ cmd/ibanator/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sula0/ibanator/v2/cmd/ibanator/docs"
 	"github.com/sula0/ibanator/v2/pkg/iban"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"net/http"
 )
 
@@ -12,10 +15,20 @@ type Response struct {
 }
 
 type IBANValidationResponse struct {
-	Valid         bool   `json:"valid"`
+	Valid         bool   `json:"valid:"`
 	InvalidReason string `json:"invalidReason,omitempty"`
 }
 
+// @BasePath /
+// @Summary Validates IBAN
+// @Tags IBAN validation
+// @Description Runs validation on IBAN. The response gives the validation result, and, if the validation failed, the reason for the failure.
+// @Accept json
+// @Produce json
+// @Param iban body string true "AL35202111090000000001234567" "AL35202111090000000001234567"
+// @Success 200 {object} Response{data=IBANValidationResponse} "Response wrapper"
+// @Failure 400  {object} Response{error=string} "Response wrapper containing only an error string"
+// @Router /iban/validate [post]
 func validateIBAN(c *gin.Context) {
 	rawData, err := c.GetRawData()
 	if err != nil || len(rawData) == 0 {
@@ -34,9 +47,18 @@ func validateIBAN(c *gin.Context) {
 	c.JSON(http.StatusOK, Response{Data: validationResponse})
 }
 
+// @title       IBANator
+// @description Service for validating IBAN.
+
+// @host      localhost:8080
+// @BasePath  /
 func main() {
 	r := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/"
+
 	r.POST("/iban/validate", validateIBAN)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	err := r.Run()
 	if err != nil {
